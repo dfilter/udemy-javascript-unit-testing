35f1165bdd345ebeceed1298a0e0d1e155e34fe8
dirk <dirk@maplelane.ag>
2025-04-04 08:35:05 -0400

    fix add function be discovering bugs with it via testing

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -15,3 +15,29 @@ it("should summarize all number values in an array", () => {
   // Assert
   expect(sum).toBe(expectedResults);
 });
+
+it("should yield NaN if at least on invalid number is provided", () => {
+  // Arrange
+  const inputs = ["invalid", 1];
+
+  // Act
+  const result = add(inputs);
+
+  // Assert
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values are provided", () => {
+  // Arrange
+  const numbers = ["1", "2"];
+  const expectedResults = numbers.reduce(
+    (previous, current) => +previous + +current,
+    0
+  );
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBe(expectedResults);
+});
