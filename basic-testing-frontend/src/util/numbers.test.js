43bca4bb3cdd470a40c5d263ae21beccbc1c693b
dirk <dirk@maplelane.ag>
2025-04-04 09:34:30 -0400

    expand tests

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -7,16 +7,32 @@ it("should return a number when passed numeric string", () => {
 
   const result = transformToNumber(stringInput);
 
-  expect(result).toBe(numberResult);
   expect(result).toBeTypeOf("number");
+  expect(result).toBe(numberResult);
 });
 
 it("should return NaN for a non-numeric string", () => {
   const stringInput = "not a number";
+  const objectInput = {};
 
   const result = transformToNumber(stringInput);
+  const resultObj = transformToNumber(objectInput);
 
   expect(result).toBeNaN();
+  expect(resultObj).toBeNaN();
+});
+
+it("should return a number for array and boolean inputs", () => {
+  const booleanInput = false;
+  const expectedBooleanResult = 0;
+  const arrayInput = [];
+  const expectedArrayResult = 0;
+
+  const resultBool = transformToNumber(booleanInput);
+  const resultArray = transformToNumber(arrayInput);
+
+  expect(resultBool).toBe(expectedBooleanResult);
+  expect(resultArray).toBe(expectedArrayResult);
 });
 
 it("should still work with multiple parameters", () => {
