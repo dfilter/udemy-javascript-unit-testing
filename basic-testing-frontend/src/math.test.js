8fc3ea6389cb3ee747eb23537761705a957a0789
dirk <dirk@maplelane.ag>
2025-04-04 08:26:33 -0400

    simplify the test numbers array

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,7 +3,7 @@ import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
   // Arrange
-  const numbers = [1, 2, 3, 4];
+  const numbers = [1, 2];
   const expectedResults = numbers.reduce(
     (previous, current) => previous + current,
     0
