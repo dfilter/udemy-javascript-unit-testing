693ceae2cf5fc70addb6a7b2d6fe37abdcd59176
dirk <dirk@maplelane.ag>
2025-04-04 08:25:39 -0400

    introducing the AAA pattern

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,6 +2,16 @@ import { expect, it } from "vitest";
 import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
-  const sum = add([1, 2, 3, 4]);
-  expect(sum).toBe(10);
+  // Arrange
+  const numbers = [1, 2, 3, 4];
+  const expectedResults = numbers.reduce(
+    (previous, current) => previous + current,
+    0
+  );
+
+  // Act
+  const sum = add(numbers);
+
+  // Assert
+  expect(sum).toBe(expectedResults);
 });
